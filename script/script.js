e8d6f5c023410b48b8f449a5481d9d0b43236a48
GDMeira <gabrieldega970@gmail.com>
2023-04-19 17:41:58 -0300

    tela 1 na metade hehe

--- script/script.js
+++ script/script.js
@@ -7,7 +7,8 @@ showQuizzes();
 
 function getAllQuizzes() { 
     const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
-    promise.then(response => allQuizzes = response.data);
+    
+    return promise;
 }
 
 function getMyQuizzes() {
@@ -21,34 +22,45 @@ function showQuizzes() {
     getMyQuizzes();
 
     if (myQuizzes) { //Se myQuizzes não for vazio ou undefined
-        document.querySelector('.myQuizzes').style.display = 'flex';
-        document.querySelector('.makeAQuizz').style.display = 'none';
+        document.querySelector('.tela1 .myQuizzes').style.display = 'flex';
+        document.querySelector('.tela1 .makeAQuizz').style.display = 'none';
     } else {
-        document.querySelector('.myQuizzes').style.display = 'none';
-        document.querySelector('.makeAQuizz').style.display = 'flex';
+        document.querySelector('.tela1 .myQuizzes').style.display = 'none';
+        document.querySelector('.tela1 .makeAQuizz').style.display = 'flex';
     }
 
-    let arrayMyQuizzes = [];
-    let arrayNotMyQuizzes = [];
-    getAllQuizzes();
-    separateMyquizzesandNotMyQuizzes(arrayMyQuizzes, arrayNotMyQuizzes);
+    const promise = getAllQuizzes();
+    promise.then(response => {
+        allQuizzes = response.data;
+
+        let arrayMyQuizzes = [];
+        let arrayNotMyQuizzes = [];
+        separateMyquizzesandNotMyQuizzes(arrayMyQuizzes, arrayNotMyQuizzes);
+
+        if (myQuizzes) {
+            renderQuizzes('.tela1 .myQuizzes ul', arrayMyQuizzes);
+        }
+        
+        renderQuizzes('.tela1 .allQuizzes ul', arrayNotMyQuizzes);
+    }).catch(error => {
+        alert(`Erro ao tentar acessar o servidor\n
+              ${error.response.status}\n
+              ${error.response.statusMessage}`);
+
+        window.location.reload();
+    })
     
-    if (myQuizzes) {
-        renderQuizzes('.myQuizzes ul', arrayMyQuizzes);
-    }
-
-    renderQuizzes('.allQuizzes ul', arrayMyQuizzes);
 }
 
 function separateMyquizzesandNotMyQuizzes(arrayMyQuizzes, arrayNotMyQuizzes) {
     if (myQuizzes) { //Se myQuizzes não for vazio ou undefined
         allQuizzes.forEach(quiz => {
-                                        if(myQuizzes.some(myQuizId => quiz.id === myQuizId.id)) {
-                                            arrayMyQuizzes.push(quiz);
-                                        } else {
-                                            arrayNotMyQuizzes.push(quiz);
-                                        }
-                                    });
+            if(myQuizzes.some(myQuizId => quiz.id === myQuizId.id)) {
+                arrayMyQuizzes.push(quiz);
+            } else {
+                arrayNotMyQuizzes.push(quiz);
+            }
+        });
     } else {
         arrayNotMyQuizzes = allQuizzes;
     }
@@ -59,11 +71,11 @@ function renderQuizzes(adress, Quizzes) {
     container.innerHTML = '';
 
     Quizzes.forEach(quiz => {
-                                container.innerHTML += `
-                                    <li class="quiz sobreposition">
-                                        <img src=${quiz.image}>
-                                        <h2>${quiz.title}</h2>
-                                    </li>
-                                `;
-                             })
+        container.innerHTML += `
+            <li class="quiz sobreposition">
+                <img src=${quiz.image}>
+                <h2>${quiz.title}</h2>
+            </li>
+        `;
+        })
 }
