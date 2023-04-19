737dcde09f56f4c40ca43d7c0031b2995a1f0dd9
GDMeira <gabrieldega970@gmail.com>
2023-04-19 10:18:29 -0300

    elementos bases para boa integração

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
 axios.defaults.headers.common['Authorization'] = '6egTHlqTKMxEUcF23T3ePOB9';
+
+let allQuizzes;
+let myQuizzes;
+
+function getAllQuizzes() { 
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
+    promise.then(response => allQuizzes = response.data);
+}
