57a40b799e02b35c66958271d5d592af7fbb07be
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-04-20 13:06:03 -0300

    função playquizz e showscreen

--- script/script.js
+++ script/script.js
@@ -5,6 +5,25 @@ let myQuizzes;
 
 showQuizzes();
 
+showScreen(2);
+
+
+function playQuizz(quizzSelecionado){
+    showScreen(2)
+    // pegar o nome do quiz // h2 quiz selecionado
+    // pesquisar o quiz no all quizzes usando o title // hof find
+    // depois que achou, pegar o objeto para renderizar o objeto na tela 
+    // onclick - mudar o display
+    // contabilizar os acertos
+    // comparar com o numero quizz
+    // montar a porcentagem de acerto
+    // display a comparacao e sucesso ou nao
+    // habilita dois botoes 1- reset pro proprio quizz 2- se clicar ele volta pra showquizzes
+    
+
+
+}
+
 function showScreen(n) {
     if (n === 1) {
         document.querySelector('.tela1').style.display = 'flex';
